6f8c0f09b3ed42fb168ec45b7ffa552059728e9d
jesus <aguilarjeremy023@upn.pe>
2025-04-09 08:34:16 -0500

    mi primer repoisitorio

---
+++ app 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("bienvenidos a las clases de c#");
+            Console.ReadKey();
+
+
+        }
+    }
+}
